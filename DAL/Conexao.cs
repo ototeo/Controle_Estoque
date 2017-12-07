a38c1b083e8187099c6e0b4fa534e38eaa41e888
Evandro Santos <evandro1android2016@gmail.com>
2017-12-07 13:04:39 -0200

    Criação dos arquivos necessários + conexão com o MySql

---
+++ DAL/Conexao.cs
@@ -0,0 +1,14 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Conexao
+    {
+        MySqlConnection connection = new MySqlConnection("Server=localhost;Database=estoque;Uid=root;Pwd=122333");
+    }
+}
