bbcce3f6b5b8948007f2b88c88a496fbdba49f14
Evandro Santos <evandro1android2016@gmail.com>
2018-01-01 21:26:44 -0200

    Operacoes

--- DAL/Conexao.cs
+++ DAL/Conexao.cs
@@ -10,5 +10,10 @@ namespace DAL
     public class Conexao
     {
         MySqlConnection connection = new MySqlConnection("Server=localhost;Database=controle_estoque;Uid=root;Pwd=122333");
+
+        public MySqlConnection getConexao()
+        {
+            return this.connection;
+        }
     }
 }
---
+++ DAL/Operacoes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace DAL
+{
+    public class Operacoes
+    {
+        private MySqlConnection connection = null;
+
+        private void abreConexao()
+        {
+            if (this.connection.State == ConnectionState.Closed)
+            {
+                this.connection.Open();
+            }
+        }
+
+        private void fechaConexao()
+        {
+            if (this.connection.State == ConnectionState.Open)
+            {
+                this.connection.Close();
+            }
+        }
+
+        public Operacoes(MySqlConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public void insert()
+        {
+            this.abreConexao();
+
+            this.fechaConexao();
+        }
+    }
+}
